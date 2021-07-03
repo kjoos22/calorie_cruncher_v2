fa0ea24978e9e58c72ba90081880b56035a88754
kjoos22 <kjoos22@gmail.com>
2021-07-03 19:26:28 -0400

    updated welcome styling

--- cc_frontend/src/components/Welcome.js
+++ cc_frontend/src/components/Welcome.js
@@ -3,10 +3,13 @@ import React from 'react'
 const Welcome = () => {
     return (
         <div>
-            <h1 style={{color: "Green"}}>Calorie Cruncher</h1>
+            <h1 style={{color: "Green", 'text-decoration': "underline", 'text-align': 'center'}}>
+                Calorie Cruncher
+            </h1>
             <h3>
-            Welcome to Calorie Cruncher, an app for tracking your daily calories
-             in and out to help you in your goals, be it gain or loss!
+                Welcome to Calorie Cruncher, an app for tracking your daily
+                calories in and out to help you in your goals, be it gain or
+                loss!
             </h3>
         </div>
     )
