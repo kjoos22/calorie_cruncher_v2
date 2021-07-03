b3f74bcc31909f36c8e1ae1fdcde4231463ec2cd
kjoos22 <kjoos22@gmail.com>
2021-07-03 13:55:09 -0400

    added links to people index

--- cc_frontend/src/components/PeopleList.js
+++ cc_frontend/src/components/PeopleList.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {Route, Link} from 'react-router-dom'
 import Person from './PersonShow'
 
 //deconstructed props
 const PeopleList = ({people}) => {
     return(
         <div>
-            {people.map(p => <li key={p.id}>{p.name} - {p.age} - {p.gender}</li>
+            {people.map(p => <li key={p.id}>
+                <Link to={`/people/${p.id}`}>{p.name} - {p.age} - {p.gender}
+                </Link>
+            </li>
             )}
         </div>
     )
