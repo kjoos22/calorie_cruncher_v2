501d1ed4bc398d885a9211c28076e114499b8a08
kjoos22 <kjoos22@gmail.com>
2021-07-03 15:06:34 -0400

    added transactions container

--- cc_frontend/src/components/PersonShow.js
+++ cc_frontend/src/components/PersonShow.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import {Redirect} from 'react-dom'
+import DaysContainer from '../containers/DaysContainer'
 
 const Person = (props) => {
     
     let person = props.people[props.match.params.id - 1]
     
     return (
-        <li>
-            {person ? person.name : null} - {person ? person.age : null}
-            - {person ? person.gender : null}
-        </li>
+        <div>
+            <h1>
+                {person ? person.name : null} - {person ? person.age : null}
+                - {person ? person.gender : null}
+            </h1>
+            <DaysContainer/>
+        </div>
     )
 }
 
---
+++ cc_frontend/src/containers/DaysContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class DaysContainer extends React.Component {
+    render () {
+        return (
+            <div>
+                TRANSACTIONS CONTAINER TEST
+            </div>
+        )
+    }
+
+
+}
+
+export default DaysContainer
